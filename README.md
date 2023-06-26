70bcce5d07693cb202fadf02395f835c9632c69d
Niranjan-KS <131178323+niranjan-ks-pro@users.noreply.github.com>
2023-06-26 16:14:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# YesTechCardWidgetPart12
+ httpswww.youtube.comwatchv=qLT7ddkvZkg&list=PL1JrLEBAapUWv_JSolgE5pdcDKWw80wKA&index=12
---
+++ android/app/src/main/kotlin/com/example/yestechcardwidgetpart12/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yestechcardwidgetpart12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
